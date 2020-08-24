86409a173d7edd35b12d7d155d1050bd5d1d6319
Albert Barrientos <albert.bar@gmail.com>
2020-08-24 18:04:26 -0400

    Deploy website - based on ac5284e3fc432394c872acce24828394421e6983

---
+++ 8be5b89e.a69f5f02.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{59:function(a){a.exports=JSON.parse('{"react":{"allTagsPath":"/blog/tags","slug":"react","name":"react","count":1,"permalink":"/blog/tags/react"},"react-router":{"allTagsPath":"/blog/tags","slug":"react-router","name":"react router","count":1,"permalink":"/blog/tags/react-router"}}')}}]);
